9d54160265b60ac3ffc63622487d96dc9e0004c4
Hejdyy <hejducat@email.cz>
2024-10-24 16:33:34 +0200

    #4 vstup-od-uzivatele

--- IS-Projekty/program004-generovani-cisel/Program.cs
+++ IS-Projekty/program004-generovani-cisel/Program.cs
@@ -3,24 +3,41 @@
         
             Console.Clear();
             Console.WriteLine("**************************");
-            Console.WriteLine("***Výpis z číselné řady***");
+            Console.WriteLine("*Generátor pseudonáhodných čísel*");
             Console.WriteLine("**************************");
             Console.WriteLine("***********Hejdy**********");
             Console.WriteLine("**************************");
             Console.WriteLine();
             Console.WriteLine();
  
-            Console.Write("Zadejte číslo (celé číslo): ");
-            int number;
-            while(!int.TryParse(Console.ReadLine(), out number)) {
+            Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
+            int n;
+            while(!int.TryParse(Console.ReadLine(), out n)) {
                 Console.Write("Nezadali jste celé číslo. Zadejte číslo (celé číslo: ");
+            }
+
+
+            Console.Write("Zadejte dolní mez čísel(celé číslo): ");
+            int dm;
+            while(!int.TryParse(Console.ReadLine(), out dm)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte dolní mez znovu (celé číslo: ");
+            }
+
+
+            Console.Write("Zadejte horní mez čísel (celé číslo): ");
+            int hm;
+            while(!int.TryParse(Console.ReadLine(), out hm)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte horní mez znovu (celé číslo: ");
             }
 
             Console.WriteLine();
             Console.WriteLine("===========================================================");
             Console.WriteLine("Zadané hodnoty: ");
-            Console.WriteLine("Číslo: {0}", number);
-            Console.WriteLine("===========================================================");
+            Console.WriteLine("Počet generovaných čísel: {0}; Dolní mez{1}; Horní mez{2}", n, dm, hm);
+            Console.WriteLine("===========================================================\n\n");
+
+
+            
 
 
             Console.WriteLine();
