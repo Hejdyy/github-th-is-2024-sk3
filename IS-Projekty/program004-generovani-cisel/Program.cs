c6fda1752218f541862185d9a9eff2b6395d4f92
Hejdyy <hejducat@email.cz>
2024-10-24 16:39:26 +0200

    #4 vstup od uzivatele

--- IS-Projekty/program004-generovani-cisel/Program.cs
+++ IS-Projekty/program004-generovani-cisel/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿        string again  = "a";
+        while (again == "a"){
+        
+            Console.Clear();
+            Console.WriteLine("**************************");
+            Console.WriteLine("*Generátor pseudonáhodných čísel*");
+            Console.WriteLine("**************************");
+            Console.WriteLine("***********Hejdy**********");
+            Console.WriteLine("**************************");
+            Console.WriteLine();
+            Console.WriteLine();
+ 
+            Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
+            int n;
+            while(!int.TryParse(Console.ReadLine(), out n)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte číslo (celé číslo: ");
+            }
+
+
+            Console.Write("Zadejte dolní mez čísel(celé číslo): ");
+            int dm;
+            while(!int.TryParse(Console.ReadLine(), out dm)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte dolní mez znovu (celé číslo: ");
+            }
+
+
+            Console.Write("Zadejte horní mez čísel (celé číslo): ");
+            int hm;
+            while(!int.TryParse(Console.ReadLine(), out hm)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte horní mez znovu (celé číslo: ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("===========================================================");
+            Console.WriteLine("Zadané hodnoty: ");
+            Console.WriteLine("Počet generovaných čísel: {0}; Dolní mez{1}; Horní mez{2}", n, dm, hm);
+            Console.WriteLine("===========================================================\n\n");
+
+
+
+
+            
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Program muzete opakovat stiskem klavesy 'a' ");
+            again = Console.ReadLine();
+
+        }  
