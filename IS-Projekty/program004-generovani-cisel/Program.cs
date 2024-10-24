a9c28a0cade94e0ca66f0c8026a9914b10db8d70
Hejdyy <hejducat@email.cz>
2024-10-24 16:27:32 +0200

    #4 generovani cisel

---
+++ IS-Projekty/program004-generovani-cisel/Program.cs
@@ -0,0 +1,31 @@
+﻿        string again  = "a";
+        while (again == "a"){
+        
+            Console.Clear();
+            Console.WriteLine("**************************");
+            Console.WriteLine("***Výpis z číselné řady***");
+            Console.WriteLine("**************************");
+            Console.WriteLine("***********Hejdy**********");
+            Console.WriteLine("**************************");
+            Console.WriteLine();
+            Console.WriteLine();
+ 
+            Console.Write("Zadejte číslo (celé číslo): ");
+            int number;
+            while(!int.TryParse(Console.ReadLine(), out number)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte číslo (celé číslo: ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("===========================================================");
+            Console.WriteLine("Zadané hodnoty: ");
+            Console.WriteLine("Číslo: {0}", number);
+            Console.WriteLine("===========================================================");
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Program muzete opakovat stiskem klavesy 'a' ");
+            again = Console.ReadLine();
+
+        }  
