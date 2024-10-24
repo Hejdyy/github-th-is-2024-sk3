2857faed464be1da4ee60a84a171055f2bc86342
Hejdyy <hejducat@email.cz>
2024-10-24 16:55:12 +0200

    #4 zakladni generator

--- IS-Projekty/program004-generovani-cisel/Program.cs
+++ IS-Projekty/program004-generovani-cisel/Program.cs
@@ -1,4 +1,6 @@
-﻿        string again  = "a";
+﻿using System.Runtime.CompilerServices;
+
+string again  = "a";
         while (again == "a"){
         
             Console.Clear();
@@ -13,21 +15,21 @@
             Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
             int n;
             while(!int.TryParse(Console.ReadLine(), out n)) {
-                Console.Write("Nezadali jste celé číslo. Zadejte číslo (celé číslo: ");
+                Console.Write("Nezadali jste celé číslo. Zadejte číslo (celé číslo): ");
             }
 
 
-            Console.Write("Zadejte dolní mez čísel(celé číslo): ");
+            Console.Write("Zadejte dolní mez čísel (celé číslo): ");
             int dm;
             while(!int.TryParse(Console.ReadLine(), out dm)) {
-                Console.Write("Nezadali jste celé číslo. Zadejte dolní mez znovu (celé číslo: ");
+                Console.Write("Nezadali jste celé číslo. Zadejte dolní mez znovu (celé číslo): ");
             }
 
 
             Console.Write("Zadejte horní mez čísel (celé číslo): ");
             int hm;
             while(!int.TryParse(Console.ReadLine(), out hm)) {
-                Console.Write("Nezadali jste celé číslo. Zadejte horní mez znovu (celé číslo: ");
+                Console.Write("Nezadali jste celé číslo. Zadejte horní mez znovu (celé číslo): ");
             }
 
             Console.WriteLine();
@@ -37,9 +39,20 @@
             Console.WriteLine("===========================================================\n\n");
 
 
+            //deklarace pole o velikosti n prvků 
+            int[] myArray = new int[n];
 
+            //příprava pro generátor náhodných čísel
+            Random randomnumber = new Random();
 
             
+            for(int i=0;i<n;i++){
+                
+                myArray[i] = randomnumber.Next(dm,hm+1);
+                Console.Write("{0}; ",myArray[i]);
+                
+            }
+
 
 
             Console.WriteLine();
